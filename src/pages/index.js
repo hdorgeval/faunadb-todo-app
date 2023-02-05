6521ad3ac81ec17ef49ac29371f7ec992d72de6c
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-02-05 23:29:53 +0100

    feat(auth): implement netlify-identity-widget

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import { Button, Container, Flex, Heading, ThemeProvider } from 'theme-ui';
+import React, { useEffect, useState } from 'react';
+import { Button, Container, Flex, Heading, NavLink, ThemeProvider } from 'theme-ui';
 import {deep}  from '@theme-ui/presets';
+import netlifyIdentity from 'netlify-identity-widget'
+import { Link } from '@reach/router';
 
-export default props => (
+
+export default props => {
+    const [user, setUser] = useState();
+    useEffect(() => {
+        netlifyIdentity.init()
+        netlifyIdentity.on("login", user => setUser(user))
+        netlifyIdentity.on("logout", user => setUser())
+    
+    })
+    return (
     <ThemeProvider theme={deep}>
         <Container>
+            <Flex as="nav">
+                <NavLink as={Link} to="/" p={2}>Home</NavLink>
+                <NavLink as={Link} to="/app" p={2}>Dashboard</NavLink>
+                <NavLink as={Link} to="/" p={2}>
+                {user && (
+                    <NavLink href="#!" p={2}>{user.user_metadata.full_name}</NavLink>)}
+                </NavLink>
+            </Flex>
             <Flex sx={{flexDirection: "column", padding: '3'}}>
                 <Heading as="h1">Get stuff done</Heading>
                 <Button sx={{marginTop: 2}}
-                    onClick={() => alert('cicked')}
+                    onClick={() => netlifyIdentity.open() }
                 >Log In</Button>
             </Flex>
     </Container>
     </ThemeProvider>
     
 )
+}
