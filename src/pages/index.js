e9c0c8b996988430e85ce394eb1de6539f8009b2
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-02-05 21:15:07 +0100

    feat(app): start landing page with theme-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Button, Container, Flex, Heading, ThemeProvider } from 'theme-ui';
+import {deep}  from '@theme-ui/presets';
 
 export default props => (
-    <div>
-        <h1>Our site</h1>
-    </div>
+    <ThemeProvider theme={deep}>
+        <Container>
+            <Flex sx={{flexDirection: "column", padding: '3'}}>
+                <Heading as="h1">Get stuff done</Heading>
+                <Button sx={{marginTop: 2}}
+                    onClick={() => alert('cicked')}
+                >Log In</Button>
+            </Flex>
+    </Container>
+    </ThemeProvider>
+    
 )
